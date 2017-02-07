cc8e0360e96516a6c54b523e7fa22fda639b95ec
CrecQi <734694749@qq.com>
2017-02-07 13:49:26 +0800

    Print all dates of the given month
    I use a generator, with is fast and clean

--- 3.14.3.py
+++ 3.14.3.py
@@ -1,8 +1,12 @@
 from datetime import datetime, timedelta
+import sys
 def date_range(start, stop, step):
 	while start < stop:
 		yield start
 		start += step
 
-for day in date_range(datetime(2012,9,1), datetime(2012,10,1), timedelta(hours = 24)):
+print "Input dates like 2012,9,1:\n"
+time_0 = int(input("The start date:"))
+time_1 = int(input("The first day of the next month:"))
+for day in date_range(datetime(time_0), datetime(time_1), timedelta(hours = 24)):
 	print day
